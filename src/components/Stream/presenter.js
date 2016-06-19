b59b090050e445ad28519c5736940f4ae8fdd6f4
Nick Hayden <nick@nickphayden.com>
2016-06-19 21:39:53 +1000

    Soundcloud authentication

--- src/components/Stream/presenter.js
+++ src/components/Stream/presenter.js
@@ -3,11 +3,16 @@ import React from 'react';
 function Stream({ tracks = [] }) {
   return (
     <div>
-      {
-        tracks.map((track, key) => {
-          return <div className="track" key={key}>{track.title}</div>;
-        })
-      }
+      <div>
+        <button onClick={onAuth} type="button">Login</button>
+      </div>
+      <div>
+        {
+          tracks.map((track, key) => {
+            return <div className="track" key={key}>{track.title}</div>;
+          })
+        }
+      </div>
     </div>
   );
 }
